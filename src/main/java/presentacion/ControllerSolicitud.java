eb8db01b30813acadb6b72b72e00427d3e1c776a
Juan <juanseca.94@gmail.com>
2016-10-07 12:29:36 -0500

    funcionando pestañas de solicitudes y muestra filtrado por estado... falta funcionalidad correcta de cada boton

--- src/main/java/presentacion/ControllerSolicitud.java
+++ src/main/java/presentacion/ControllerSolicitud.java
@@ -17,19 +17,22 @@ public class ControllerSolicitud {
 			ControllerSolicitud.inicio = false;
 			String[] convocante = {"Luis Alberto Rodríguez"}; //Tener en cuenta que al añadir mas, se desconfigura la vista
 			String[] convocado = {"Alfonso López"};
-			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("29/09/2016",convocante,convocado,"Por Reparto","Breve descripcion...","radicar"));
+			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("29/09/2016",convocante,convocado,"Por Reparto","Breve descripcion...","Radicar"));
 			String[] convocante2 = {"Luis Alberto Rodríguez"};
 			String[] convocado2 = {"Alfonso López"};
-			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("28/09/2016",convocante2,convocado2,"2345673409","Breve descripcion...","radicar"));
+			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("28/09/2016",convocante2,convocado2,"2345673409","Breve descripcion...","Radicar"));
 			String[] convocante3 = {"Luis Alberto Rodríguez"};
 			String[] convocado3 = {"Alfonso López"};
-			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("30/09/2016",convocante3,convocado3,"87655543","Breve descripcion...","liquidar"));
+			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("30/09/2016",convocante3,convocado3,"87655543","Breve descripcion...","Liquidar"));
 			String[] convocante4 = {"Luis Alberto Rodríguez"};
 			String[] convocado4 = {"Alfonso López"};
-			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("03/10/2016",convocante4,convocado4,"Por Reparto","Breve descripcion...","liquidar"));
+			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("03/10/2016",convocante4,convocado4,"Por Reparto","Breve descripcion...","Liquidar"));
 			String[] convocante5 = {"Luis Alberto Rodríguez"};
 			String[] convocado5 = {"Alfonso López"};
-			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("06/10/2016",convocante5,convocado5,"Por Reparto","Breve descripcion...","radicar"));
+			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("06/10/2016",convocante5,convocado5,"Por Reparto","Breve descripcion...","Radicar"));
+			String[] convocante6 = {"Luis Alberto Rodríguez"};
+			String[] convocado6 = {"Alfonso López"};
+			ControllerSolicitud.listaSolicitud.add(new ModelSolicitud("06/10/2016",convocante6,convocado6,"Por Reparto","Breve descripcion...","Audiencia"));
 		}
 	}
 
@@ -37,40 +40,16 @@ public class ControllerSolicitud {
 		return ControllerSolicitud.listaSolicitud;
 	}
 	
-	public List<ModelSolicitud> solicitudesLiquidar(){
-		List<ModelSolicitud> listaPorEstado = new ArrayList<ModelSolicitud>();
-		int size = ControllerSolicitud.listaSolicitud.size();
-		
-		for(int i=0; i<size; i++){
-			if(ControllerSolicitud.listaSolicitud.get(i).getEstado().equals("liquidar")){
-				listaPorEstado.add(ControllerSolicitud.listaSolicitud.get(i));
-			}			
-		}
-		
-		return listaPorEstado;
-	}
-	
-	public List<ModelSolicitud> solicitudesRadicar(){
-		List<ModelSolicitud> listaPorEstado = new ArrayList<ModelSolicitud>();
-		int size = ControllerSolicitud.listaSolicitud.size();
-		
-		for(int i=0; i<size; i++){
-			if(ControllerSolicitud.listaSolicitud.get(i).getEstado().equals("radicar")){
-				listaPorEstado.add(ControllerSolicitud.listaSolicitud.get(i));
-			}			
-		}
-		
-		return listaPorEstado;
-	}
-	
 	public List<ModelSolicitud> solicitudesPorEstado(String estado){
-		List<ModelSolicitud> listaPorEstado = ControllerSolicitud.listaSolicitud;
-		int size = listaPorEstado.size();
-		for (int i = 0; i < size; i++) {
-			if(!listaPorEstado.get(i).getEstado().equals(estado)){
-				listaPorEstado.remove(i);
-			}
+		List<ModelSolicitud> listaPorEstado = new ArrayList<ModelSolicitud>();
+		int size = ControllerSolicitud.listaSolicitud.size();
+		
+		for(int i=0; i<size; i++){
+			if(ControllerSolicitud.listaSolicitud.get(i).getEstado().equals(estado)){
+				listaPorEstado.add(ControllerSolicitud.listaSolicitud.get(i));
+			}			
 		}
+		
 		return listaPorEstado;
 	}
 }
