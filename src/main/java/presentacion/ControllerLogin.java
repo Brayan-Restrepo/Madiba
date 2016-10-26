21c3e1ad4b07c11cc1f6888b377ee8d786a02799
Brayan-Restrepo <brayan.restrepo@unillanos.edu.co>
2016-10-25 23:00:49 -0500

    captcha en Español

--- src/main/java/presentacion/ControllerLogin.java
+++ src/main/java/presentacion/ControllerLogin.java
@@ -8,8 +8,12 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 public class ControllerLogin {
 	
-	 public void submit() {	
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Correct", "Correct");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+	public void submit() {	
+			
+		System.out.println("....");
+		    FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Correct", "Correct");
+		    FacesContext.getCurrentInstance().addMessage(null, msg);
+		
     }
 }
