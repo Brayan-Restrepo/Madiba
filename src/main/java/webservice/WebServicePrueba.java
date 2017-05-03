f83a10972db5cb6f4a219a4ebaf0676c426a059f
Juan <juanseca.94@gmail.com>
2017-05-03 11:37:57 -0500

    Solucionado error en el Web Service...

--- src/main/java/webservice/WebServicePrueba.java
+++ src/main/java/webservice/WebServicePrueba.java
@@ -1,23 +1,14 @@
 package webservice;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.ejb.EJB;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-import entidades.Conciliador;
-import negocio.iConciliadorBean;
-
 
 @Path("/webservice")
 public class WebServicePrueba {
-	
-
 	
 	@POST
 	@Produces({"application/json"})
@@ -25,27 +16,14 @@ public class WebServicePrueba {
     		@QueryParam("user") String user,
     		@QueryParam("pass") String pass
     ) {
-		List<Conciliador> c = new ArrayList<Conciliador>();
-		Conciliador cons = new Conciliador();
-		cons.setApellidos("fffff");
-		c.add(cons);
 		return Response
 				.status(200)
 				.header("Access-Control-Allow-Origin", "*")
 			    .header("Access-Control-Allow-Credentials", "true")
 			    .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-			     .header("Access-Control-Allow-Methods", 
-			        "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-			     .entity(c)
-			     .build();
-		//return $login(user,pass);
-        /*return Response.ok($login(user,pass))
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .build();*/
+			    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+			    .entity($login(user,pass))
+			    .build();
     }
 
     String $login(String user, String pass) {
@@ -56,9 +34,9 @@ public class WebServicePrueba {
 			resultado = "INVALIDO";
 		}
         
-    	return    "{" 
-    			+ "\"resultado\":"+resultado+","
-    			+ "\"dato\":"+user
-    			+ "}";
+    	return  "{" 
+				+ "\"resultado\":\""+resultado+"\","
+				+ "\"dato\":\""+user+"\""
+				+ "}";
     }
 }
