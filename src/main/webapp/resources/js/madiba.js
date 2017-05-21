6a325d646fc10a30aed300b86fe17aee0dae12f4
Brayan-Restrepo <brayan.restrepo@unillanos.edu.co>
2017-05-21 18:24:57 -0500

    REVISION 2 -- SE OCULTO COMPONENTE DE BUSQUEDAS AL CLIC EN MENU

--- src/main/webapp/resources/js/madiba.js
+++ src/main/webapp/resources/js/madiba.js
@@ -9,4 +9,17 @@ $(document).ready(function() {
     $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
     //Money Euro
     $("[data-mask]").inputmask();
+    
+    $("#ocultarBusqueda").click(function(){
+    	
+    	var oculto = $(document.getElementById('formBusqueda:divBusquedas')).css('display');
+    	console.log(oculto);
+    	if(oculto == "none"){
+    		$(document.getElementById('formBusqueda:divBusquedas')).show("swing");
+    		$(document.getElementById('formBusqueda:mensaje')).show("swing");
+    	}else if(oculto == "block" || oculto=="inline-block"){
+    		$(document.getElementById('formBusqueda:divBusquedas')).hide("linear");
+    		$(document.getElementById('formBusqueda:mensaje')).hide("linear");
+    	}
+	 });
 });
